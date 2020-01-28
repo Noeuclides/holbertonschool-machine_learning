e885025d510d6e85b3abfea75f6d86e58076adb0
No Euclides <euclidesnoeuclides@gmail.com>
2020-01-28 05:50:15 -0500

    matrix multiplication with numpy

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module to multiplicate matrices on numpy
+"""
+#import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Performs matrix multiplication
+    """
+    return mat1 @ mat2
