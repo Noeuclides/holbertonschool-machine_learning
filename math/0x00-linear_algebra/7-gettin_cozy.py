05118c68c3b49bc4012d503684c6c50916fe74ff
Noeuclides <euclidesnoeuclides@gmail.com>
2020-01-28 18:06:28 +0000

    copying both matrices

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -9,16 +9,20 @@ def cat_matrices2D(mat1, mat2, axis=0):
     Concatenates two matrices along a specific axis
     """
     cat = []
+    cat2 = []
     for item in mat1:
         inner = item[:]
         cat.append(inner)
+    for item in mat2:
+        inner2 = item[:]
+        cat2.append(inner2)
 
-    if axis == 0 and len(mat1[0]) == len(mat2[0]):
-        for elem in mat2:
+    if axis == 0 and len(cat[0]) == len(cat2[0]):
+        for elem in cat2:
             cat.append(elem)
         return cat
-    elif axis == 1 and len(mat1) == len(mat2):
+    elif axis == 1 and len(cat) == len(cat2):
         for i in range(len(cat)):
             for j in range(1):
-                cat[i].append(mat2[i][j])
+                cat[i].append(cat2[i][j])
         return cat
