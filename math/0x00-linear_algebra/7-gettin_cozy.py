8236a49c14705e5f08beac6e2615500382d99315
Noeuclides <euclidesnoeuclides@gmail.com>
2020-01-28 18:51:11 +0000

    redo conditionals but copying only mat1

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -13,17 +13,14 @@ def cat_matrices2D(mat1, mat2, axis=0):
     for item in mat1:
         inner = item[:]
         cat.append(inner)
-    for item in mat2:
-        inner2 = item[:]
-        cat2.append(inner2)
 
-    if axis == 0:
-        for elem in cat2:
+    if axis == 0 and len(cat[0]) == len(mat2[0]):
+        for elem in mat2:
             cat.append(elem)
         return cat
-    if axis == 1:
+    if axis == 1 and len(cat) == len(mat2):
         new = []
         for i in range(len(cat)):
-            nsum = cat[i] + cat2[i]
+            nsum = cat[i] + mat2[i]
             new.append(nsum)
         return new
