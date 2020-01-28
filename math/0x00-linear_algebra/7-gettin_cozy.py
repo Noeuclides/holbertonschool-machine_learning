b41a48c4fca863cc06fa9e6f7db570d90ce08793
Noeuclides <euclidesnoeuclides@gmail.com>
2020-01-28 18:02:37 +0000

    returning None when conditionals are false

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -16,9 +16,9 @@ def cat_matrices2D(mat1, mat2, axis=0):
     if axis == 0 and len(mat1[0]) == len(mat2[0]):
         for elem in mat2:
             cat.append(elem)
+        return cat
     elif axis == 1 and len(mat1) == len(mat2):
         for i in range(len(cat)):
             for j in range(1):
                 cat[i].append(mat2[i][j])
-
-    return cat
+        return cat
