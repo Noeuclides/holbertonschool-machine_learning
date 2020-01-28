1adb1372a510717ca49150c308e067e7854d636a
Noeuclides <euclidesnoeuclides@gmail.com>
2020-01-28 18:35:31 +0000

    two if statements

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -21,7 +21,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
         for elem in cat2:
             cat.append(elem)
         return cat
-    elif axis == 1 and len(cat) == len(cat2):
+    if axis == 1 and len(cat) == len(cat2):
         for i in range(len(cat)):
             for j in range(1):
                 cat[i].append(cat2[i][j])
