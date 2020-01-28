4a5d84e153a91f5f523fb1f57061a1fe8a5beefe
Noeuclides <euclidesnoeuclides@gmail.com>
2020-01-28 18:56:35 +0000

    everything inside conditionals

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -8,19 +8,21 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """
     Concatenates two matrices along a specific axis
     """
-    cat = []
-    cat2 = []
-    for item in mat1:
-        inner = item[:]
-        cat.append(inner)
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
+        cat = []
+        for item in mat1:
+            inner = item[:]
+            cat.append(inner)
 
-    if axis == 0 and len(cat[0]) == len(mat2[0]):
         for elem in mat2:
             cat.append(elem)
+
         return cat
-    if axis == 1 and len(cat) == len(mat2):
+
+    if axis == 1 and len(mat1) == len(mat2):
         new = []
-        for i in range(len(cat)):
-            nsum = cat[i] + mat2[i]
+        for i in range(len(mat1)):
+            nsum = mat1[i] + mat2[i]
             new.append(nsum)
+
         return new
