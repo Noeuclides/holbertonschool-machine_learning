0c6843110a72f74366ef767a6d0507633f818575
Noeuclides <euclidesnoeuclides@gmail.com>
2020-01-28 17:42:01 +0000

    concat matrices 2D, copy inner arrays

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -8,18 +8,17 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """
     Concatenates two matrices along a specific axis
     """
-    cat = mat1.copy()
-    print("CAT: ", cat)
+    cat = mat1[:]
+    for i in range(len(cat)):
+        inner = cat[i][:]
+        cat[i] = inner
+
     if axis == 0:
-        print("something")
         for elem in mat2:
             cat.append(elem)
     else:
-        print("another")
         for i in range(len(cat)):
             for j in range(1):
                 cat[i].append(mat2[i][j])
-
-    print("mat1: ", mat1)
 
     return cat
