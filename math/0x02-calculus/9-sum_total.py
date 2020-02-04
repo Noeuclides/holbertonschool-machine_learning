fac34c4fa72be4ebab1e16d84dc70485ba4aad22
No Euclides <euclidesnoeuclides@gmail.com>
2020-02-04 14:06:42 -0500

    return none if n not exist

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -8,6 +8,9 @@ def summation_i_squared(n):
     """
     calculate sum
     """
+
+    if n is None or n < 1:
+        return None
     if not isinstance(n, (int, float)):
         return None
     return int(sum(map(lambda x: x ** 2, range(1, (n + 1)))))
