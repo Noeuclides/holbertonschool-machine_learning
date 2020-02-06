03fdbe285a6f6410c6a329887dbf467ec98fdd49
No Euclides <euclidesnoeuclides@gmail.com>
2020-02-06 06:51:47 -0500

    Gradient plt

--- math/0x01-plotting/100-gradient.py
+++ math/0x01-plotting/100-gradient.py
@@ -9,3 +9,10 @@ y = np.random.randn(2000) * 10
 z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
 
 # your code here
+plt.scatter(x, y, c=z)
+plt.xlabel('x coordinate (m)')
+plt.ylabel('y coordinate (m)')
+cbar = plt.colorbar()
+cbar.set_label('elevation (m)')
+plt.title('Mountain Elevation')
+plt.show()
