d6acdcf63a2c454fae976339280faa8e49f22190
No Euclides <euclidesnoeuclides@gmail.com>
2020-02-06 10:34:01 -0500

    Principle component Analysis

--- math/0x01-plotting/101-pca.py
+++ math/0x01-plotting/101-pca.py
@@ -13,3 +13,13 @@ _, _, Vh = np.linalg.svd(norm_data)
 pca_data = np.matmul(norm_data, Vh[:3].T)
 
 # your code here
+x = pca_data.T[0]
+y = pca_data.T[1]
+z = pca_data.T[2]
+ax = Axes3D(plt.figure())
+ax.scatter(x,y,z,c=labels,cmap='plasma')
+ax.set_xlabel('U1')
+ax.set_ylabel('U2')
+ax.set_zlabel('U3')
+ax.set_title('PCA of Iris Dataset')
+plt.show()
