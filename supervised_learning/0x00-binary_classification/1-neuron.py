1f8668abc57a8aed708d2a7e85e1a5598dd501ff
Noeuclides <euclidesnoeuclides@gmail.com>
2020-02-16 07:23:54 +0000

    using random.normal

--- supervised_learning/0x00-binary_classification/1-neuron.py
+++ supervised_learning/0x00-binary_classification/1-neuron.py
@@ -1,1 +1,37 @@
 #!/usr/bin/env python3
+"""module that defines a single neuron
+"""
+import numpy as np
+
+
+class Neuron:
+    """define a single neuron performing binary classification
+    """
+    def __init__(self, nx):
+        """class constructor
+        """
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+        @property
+        def W(self):
+            """setter method of the weights
+            """
+            return self.__W
+
+        @property
+        def b(self):
+            """setter method of the bias
+            """
+            return self.__b
+
+        @property
+        def A(self):
+            """setter method of the activated output
+            """
+            return self.__A
