d595ae47c98336d4f91752c1f3c9d01ed999e807
No Euclides <euclidesnoeuclides@gmail.com>
2020-02-18 23:19:23 -0500

    privatize instance attributes

--- supervised_learning/0x00-binary_classification/17-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/17-deep_neural_network.py
@@ -20,7 +20,7 @@ class DeepNeuralNetwork:
             raise ValueError('nx must be a positive integer')
         if not isinstance(layers, list):
             raise TypeError('layers must be a list of positive integers')
-        if any(item <= 0 for item in layers):
+        if len(layers) == 0:
             raise TypeError('layers must be a list of positive integers')
         self.L = len(layers)
         self.cache = {}
