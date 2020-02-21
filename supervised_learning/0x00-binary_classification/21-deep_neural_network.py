a53e7a449de22026c996edb1191bff054575eab1
No Euclides <euclidesnoeuclides@gmail.com>
2020-02-21 13:46:22 -0500

    gradient descent NN and DNN, matmul vs hadamard

--- supervised_learning/0x00-binary_classification/21-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/21-deep_neural_network.py
@@ -89,13 +89,22 @@ class DeepNeuralNetwork:
     def gradient_descent(self, Y, cache, alpha=0.05):
         """Calculate one pass of gradient descent on the neural network
         """
-        for l in range(self.__L):
-            key = 'A{}'.format(l + 1)
+        for layer in range(self.__L, 0, -1):
+            key = 'A{}'.format(layer)
+            input = 'A{}'.format(layer - 1)
+            w = 'W{}'.format(layer)
             out = 'A{}'.format(self.__L)
-            bias = 'b{}'.format(l + 1)
-            dw = np.matmul(cache[key], (cache[out] - Y).T) / Y.shape[1]
-            db = np.sum(cache[out] - Y) / Y.shape[1]
-            self.__W[key] = self.__W[key] - alpha * dw.T
-            self.__b[bias] = self.__b[bias] - alpha * db.T
-        
-        return self.__W, self.__b
+            bias = 'b{}'.format(layer)
+            if layer == self.__L:
+                dz = cache[out] - Y
+                dw = np.matmul(dz, cache[input].T) / Y.shape[1]
+            else:
+                w1 = 'W{}'.format(layer + 1)
+                back = np.matmul(self.__weights[w1].T, dz)
+                derivative = cache[key] * (1 - cache[key])
+                dz = back * derivative
+                dw = np.matmul(dz, cache[input].T) / Y.shape[1]
+            db = np.sum(dz, axis=1, keepdims=True) / Y.shape[1]
+            self.__weights[w] = self.__weights[w] - alpha * dw
+            self.__weights[bias] = self.__weights[bias] - alpha * db
+        return self.__weights
