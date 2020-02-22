66d376fcf2a1aaa0853faeafe9e18d0c918fb1dd
No Euclides <euclidesnoeuclides@gmail.com>
2020-02-22 12:03:23 -0500

    check type of classes and classes most be equal to the maximum number of classes found in Y

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -8,9 +8,11 @@ import numpy as np
 def one_hot_encode(Y, classes):
     """convert a numeric label vector into a one-hot matrix
     """
+    if not isinstance(classes, int):
+        return None
     if Y.size == 0:
         return None
-    if classes < Y.max() + 1:
+    if classes != Y.max() + 1:
         return None
     encode = np.zeros((classes, Y.shape[0]))
     rows = np.arange(Y.shape[0])
