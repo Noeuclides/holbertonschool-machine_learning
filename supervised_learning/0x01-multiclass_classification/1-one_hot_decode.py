d0c4cc4dde3ab8860bcbf406b418182258c871c8
No Euclides <euclidesnoeuclides@gmail.com>
2020-02-22 18:11:25 -0500

    check type np ndarray

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -8,6 +8,12 @@ import numpy as np
 def one_hot_decode(one_hot):
     """convert a one-hot matrix into a vector of labels
     """
+    if not isinstance(one_hot, np.ndarray):
+        return None
+    for e in one_hot:
+        if not isinstance(e, np.ndarray):
+            return None
+
     decode = np.zeros((one_hot.shape[1]), dtype=int)
     p = np.where(one_hot == 1)
     for i in range(len(one_hot)):
