0d2821ad7d0f9f945d11571dd6cbb608fb73c595
No Euclides <euclidesnoeuclides@gmail.com>
2020-02-22 07:55:32 -0500

    one hot encode and decode

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""module with a on hot encode function
+"""
+
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """convert a numeric label vector into a one-hot matrix
+    """
+    encode = np.zeros((classes, Y.shape[0]))
+    rows = np.arange(Y.shape[0])
+    encode[Y, rows] = 1
+    return encode
--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""module with a on hot encode function
+"""
+
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """convert a one-hot matrix into a vector of labels
+    """
+    decode = np.zeros((one_hot.shape[1]), dtype=int)
+    p = np.where(one_hot == 1)
+    for i in range(len(one_hot)):
+        p = np.where(one_hot[i] == 1)
+        decode[p] = i
+    return decode
