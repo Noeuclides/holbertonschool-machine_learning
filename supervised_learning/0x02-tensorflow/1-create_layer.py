ffb5160d39251b9494a4dccf612b321188acc92e
Noeuclides <euclidesnoeuclides@gmail.com>
2020-02-26 00:00:03 +0000

    layers tf

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
+"""module to get the tensor output of a layer
 """
-"""
+
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+    """the tensor output of the layer
+    """
+    layer = tf.layers.dense(prev, units=n, activation=activation, name='layer')
+    tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+
+    return layer
