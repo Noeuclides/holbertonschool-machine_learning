3c5e90c1a4153176edbc49faf449ad9f64469a5c
Noeuclides <euclidesnoeuclides@gmail.com>
2020-02-26 00:06:55 +0000

    kernel_initializer for weights

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -8,7 +8,12 @@ import tensorflow as tf
 def create_layer(prev, n, activation):
     """the tensor output of the layer
     """
-    layer = tf.layers.dense(prev, units=n, activation=activation, name='layer')
-    tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    weights_init = tf.contrib.layers.variance_scaling_initializer(
+        mode="FAN_AVG")
+    layer = tf.layers.dense(prev,
+                            units=n,
+                            activation=activation,
+                            name='layer',
+                            kernel_initializer=weights_init)
 
     return layer
