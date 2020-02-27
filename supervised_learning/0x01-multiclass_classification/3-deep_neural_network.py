aee294d898a883e4882bf0cfe80301ccacd5de27
No Euclides <euclidesnoeuclides@gmail.com>
2020-02-26 22:26:14 -0500

    update binary to multiclass

--- supervised_learning/0x01-multiclass_classification/3-deep_neural_network.py
+++ supervised_learning/0x01-multiclass_classification/3-deep_neural_network.py
@@ -60,15 +60,19 @@ class DeepNeuralNetwork:
         """Calculate the forward propagation of the neural network
         """
         self.__cache['A0'] = X
-        for l in range(self.__L):
-            key = 'W{}'.format(l + 1)
-            bias = 'b{}'.format(l + 1)
+        for layer in range(self.__L):
+            key = 'W{}'.format(layer + 1)
+            bias = 'b{}'.format(layer + 1)
             w = self.__weights[key]
-            key_cache = 'A{}'.format(l)
+            key_cache = 'A{}'.format(layer)
             cache = self.__cache[key_cache]
             z = np.matmul(w, cache) + self.__weights[bias]
-            A = 'A{}'.format(l + 1)
-            self.__cache[A] = 1 / (1 + np.exp(-z))
+            A = 'A{}'.format(layer + 1)
+            if layer == self.__L - 1:
+                den = np.sum(np.exp(z), axis=0, keepdims=True)
+                self.__cache[A] = np.exp(z) / den
+            else:
+                self.__cache[A] = 1 / (1 + np.exp(-z))
         out = 'A{}'.format(self.__L)
         return self.__cache[out], self.__cache
 
@@ -76,17 +80,17 @@ class DeepNeuralNetwork:
         """Calculate the cost of the model using logistic regression
         """
         loss1 = np.matmul(Y, np.log(A).T)
-        loss2 = np.matmul(1 - Y, np.log(1.0000001 - A.T))
         m = Y.shape[1]
-        cost = np.sum(-(loss1 + loss2)) / m
+        cost = -1 * np.sum(loss1) / m
         return cost
 
     def evaluate(self, X, Y):
         """Evaluate the neural network’s predictions
         """
         A, _ = self.forward_prop(X)
+        max = np.argmax(A)
         cost = self.cost(Y, A)
-        return np.where(A >= 0.5, 1, 0), cost
+        return np.where(max == 1, 1, 0), cost
 
     def gradient_descent(self, Y, cache, alpha=0.05):
         """Calculate one pass of gradient descent on the neural network
@@ -125,27 +129,26 @@ class DeepNeuralNetwork:
         """
         if not isinstance(iterations, int):
             raise TypeError('iterations must be an integer')
-        if iterations < 1:
+        if iterations < 0:
             raise ValueError('iterations must be a positive integer')
         if not isinstance(alpha, float):
             raise TypeError('alpha must be a float')
         if alpha < 0:
             raise ValueError('alpha must be positive')
-        if verbose and graph:
+        if verbose or graph:
             if not isinstance(step, int):
                 raise TypeError('step must be an integer')
-            if step <= 0 and step > iterations:
+            if step <= 0 or step > iterations:
                 raise ValueError('step must be positive and <= iterations')
 
         costs = []
-        iteration = []
-        for i in range(iterations + 1):
-            _, self.__cache = self.forward_prop(X)
+        iteration = np.arange(0, iterations)
+        for i in range(iterations):
+            A, self.__cache = self.forward_prop(X)
             key = "A{}".format(self.__L)
-            cost = self.cost(Y, self.__cache[key])
+            cost = self.cost(Y, A)
+            costs.append(cost)
             if i % step == 0 or i == iterations:
-                costs.append(cost)
-                iteration.append(i)
                 if verbose:
                     print("Cost after {} iterations: {}".format(i, cost))
             self.gradient_descent(Y, self.__cache, alpha)
@@ -170,8 +173,9 @@ class DeepNeuralNetwork:
     def load(filename):
         """Load a pickled DeepNeuralNetwork object
         """
-        if not filename:
+        try:
+            with open(filename, 'rb') as f:
+                data = pickle.load(f)
+            return data
+        except FileNotFoundError:
             return None
-        with open(filename, 'rb') as f:
-            data = pickle.load(f)
-        return data
