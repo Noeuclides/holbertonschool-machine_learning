08e36a11e1cdc2c49c7953ec5e280f59ba9047cf
No Euclides <euclidesnoeuclides@gmail.com>
2020-03-01 22:04:58 -0500

    No train

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -3,17 +3,35 @@
 """
 
 import tensorflow as tf
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
 
 
-def train(
-        X_train,
-        Y_train,
-        X_valid,
-        Y_valid,
-        layer_sizes,
-        activations,
-        alpha,
-        iterations,
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
+        activations, alpha, iterations,
         save_path="/tmp/model.ckpt"):
     """builds, trains, and saves a neural network classifier
     """
+    print(X_train, X_train.shape)
+    print(Y_train, Y_train.shape)
+    x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
+    y_pred = forward_prop(x, layer_sizes, activations)
+    accuracy = calculate_accuracy(y, y_pred)
+    loss = calculate_loss(y, y_pred)
+    train_op = create_train_op(loss, alpha) 
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init)
+        for i in range(iterations):
+            feed_dict = {'inputs': X_train[i], 'labels': X_valid[i]}
+            sess.run(train_op, feed_dict=feed_dict)
+            if i % 100 == 0 or i == iterations:
+                print("After {} iterations:".format(i))
+                print("\tTraining Cost: {}".format(loss))
+                print("\tTraining Accuracy: {}".format(accuracy))
+                print("\tValidation Cost: {}".format(loss))
+                print("\tValidation Accuracy: {}".format(accuracy))
+    return save_path
