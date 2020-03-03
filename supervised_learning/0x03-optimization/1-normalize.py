24a19a8a64280bebe39c1ea639111fe14857837d
No Euclides <euclidesnoeuclides@gmail.com>
2020-03-03 03:00:57 -0500

    normalize matrix

--- supervised_learning/0x03-optimization/1-normalize.py
+++ supervised_learning/0x03-optimization/1-normalize.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
+"""module to normalize
 """
-"""
+
 import tensorflow as tf
 import numpy as np
+
+
+def normalize(X, m, s):
+    """normalizes (standardizes) a matrix
+    """
+    for i in range(X.shape[0]):
+        for j in range(X.shape[1]):
+            X[i][j] = (X[i][j] - m[j]) / s[j]
+    return X
