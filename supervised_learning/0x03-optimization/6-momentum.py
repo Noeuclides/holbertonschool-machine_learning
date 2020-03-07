8156f62d77eb4c3b85ba294c4d48522f93871fef
No Euclides <euclidesnoeuclides@gmail.com>
2020-03-07 06:19:25 -0500

    momentum

--- supervised_learning/0x03-optimization/6-momentum.py
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
+"""module momentum ugraded
 """
-"""
+
 import tensorflow as tf
-import numpy as np
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """creates the training operation for a neural network
+    """
+    momentum = tf.train.MomentumOptimizer(alpha, beta1).minimize(loss)
+
+    return momentum
