dbc57754ab4cf03e426c07897d630d0b6e6b6308
No Euclides <euclidesnoeuclides@gmail.com>
2020-03-08 11:46:09 -0500

    confusion matrix and sensitivity

--- supervised_learning/0x04-error_analysis/1-sensitivity.py
+++ supervised_learning/0x04-error_analysis/1-sensitivity.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+"""module of sensitivity confusion matrix
 """
-"""
+
 import numpy as np
+
+
+def sensitivity(confusion):
+    """calculates the sensitivity for each class in a confusion matrix
+    """
+
+    sensitivity = []
+    for i in range(confusion.shape[0]):
+        true_pos = confusion[i][i]
+        false_neg = sum(confusion[i]) - true_pos
+
+        sensitivity.append(true_pos / (true_pos + false_neg))
+    return sensitivity
