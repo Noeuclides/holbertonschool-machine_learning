63475dbceed372513a21ab55ca9057663e9f313b
No Euclides <euclidesnoeuclides@gmail.com>
2020-03-11 07:12:40 -0500

    L2 regularization cost

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+"""L2 Regularization Cost
 """
-"""
+
 import numpy as np
+
+
+def l2_reg_cost(cost, lambtha, weights, L, m):
+    """
+    calculates the cost of a NN with L2 regularization
+    - cost: cost of the network without L2 regularization
+    - lambtha: regularization parameter
+    - weights: dictionary of the weights and biases (numpy.ndarrays) of the NN
+    - L: number of layers in the NN
+    - m is the number of data points used
+    """
+    for i in range(L):
+        key = "W{}".format(i + 1)
+        l2_reg = np.linalg.norm(weights[key])
+        cost = cost + lambtha * l2_reg / (2 * m)
+
+    return cost
