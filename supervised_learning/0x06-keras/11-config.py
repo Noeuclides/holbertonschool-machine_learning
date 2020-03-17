7b5316db4f0c1a65f59c7e3905feed662ca7e68e
No Euclides <euclidesnoeuclides@gmail.com>
2020-03-17 00:28:05 -0500

    load

--- supervised_learning/0x06-keras/11-config.py
+++ supervised_learning/0x06-keras/11-config.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
+"""save and load weights
 """
-"""
-import numpy as np
+
+import tensorflow.keras as K
+
+
+def save_config(network, filename):
+    """saves an entire model:
+    """
+    with open(filename, "w") as f:
+        f.write(network.to_json())
+    return None
+
+
+def load_config(filename):
+    """loads an entire model
+    """
+    with open(filename, "r") as f:
+        model_load = f.read()
+
+    model = K.models.model_from_json(model_load)
+
+    return model
