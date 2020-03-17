359542578295afe3279076fdee8aefdf40e76d32
No Euclides <euclidesnoeuclides@gmail.com>
2020-03-17 00:05:54 -0500

    save and load

--- supervised_learning/0x06-keras/9-model.py
+++ supervised_learning/0x06-keras/9-model.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
+"""save and load module
 """
-"""
-import numpy as np
+
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """saves an entire model:
+    """
+    network.save(filename)
+
+    return None
+
+
+def load_model(filename):
+    """loads an entire model
+    """
+    model = K.models.load_model(filename)
+
+    return model
