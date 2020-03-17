b7f56d43ec54704de57c3b8e4accfb16fcfac944
No Euclides <euclidesnoeuclides@gmail.com>
2020-03-17 00:15:11 -0500

    save and load weights

--- supervised_learning/0x06-keras/10-weights.py
+++ supervised_learning/0x06-keras/10-weights.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
+"""save and load weights
 """
-"""
-import numpy as np
+
+import tensorflow.keras as K
+
+
+def save_weights(network, filename, save_format='h5'):
+    """saves an entire model:
+    """
+    network.save_weights(filename, save_format=save_format)
+
+    return None
+
+
+def load_weights(network, filename):
+    """loads an entire model
+    """
+    model = K.models.load_weights(filename)
+
+    return model
