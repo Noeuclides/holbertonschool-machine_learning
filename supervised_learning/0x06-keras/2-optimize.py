3e47081b38f64976daf3216a17178ced833a7039
No Euclides <euclidesnoeuclides@gmail.com>
2020-03-16 23:22:35 -0500

    optimizer adam

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -16,8 +16,5 @@ def optimize_model(network, alpha, beta1, beta2):
     opt = K.optimizers.Adam(learning_rate=alpha, beta_1=beta1, beta_2=beta2)
     network.compile(
         optimizer=opt,
-        loss=K.losses.CategoricalCrossentropy(
-            from_logits=True),
+        loss='categorical_crossentropy',
         metrics=['accuracy'])
-
-    return None
