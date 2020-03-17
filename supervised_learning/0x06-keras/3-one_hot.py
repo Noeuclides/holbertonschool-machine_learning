e2f25a7a77d2b74644b26ce8cfcdf4985264e14e
No Euclides <euclidesnoeuclides@gmail.com>
2020-03-16 23:30:16 -0500

    one hot matrix keras

--- supervised_learning/0x06-keras/3-one_hot.py
+++ supervised_learning/0x06-keras/3-one_hot.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+"""one hot matrix module
 """
-"""
-import numpy as np
+
+import tensorflow.keras as K
+
+
+def one_hot(labels, classes=None):
+    """converts a label vector into a one-hot matrix:
+    """
+    encode = K.utils.to_categorical(labels)
+
+    return encode
