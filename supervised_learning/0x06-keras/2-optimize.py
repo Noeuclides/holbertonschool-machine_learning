afaa9c8cbc34c756bbcc25b2dcc54016270b367a
No Euclides <euclidesnoeuclides@gmail.com>
2020-03-16 23:15:53 -0500

    optimizer adam

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+"""Optiize module
 """
-"""
-import numpy as np
+
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """
+    sets up Adam optimization for a keras model:
+    - network: model to optimize
+    - alpha: learning rate
+    - beta1: first Adam optimization parameter
+    - beta2: second Adam optimization parameter
+    """
+    K.optimizers.Adam(learning_rate=alpha, beta_1=beta1, beta_2=beta2)
