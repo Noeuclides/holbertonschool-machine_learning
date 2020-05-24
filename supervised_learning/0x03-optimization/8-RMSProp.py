0999e88287e73d2da5b82f527184ba80e6888169
Noeuclides <euclidesnoeuclides@gmail.com>
2020-05-24 14:54:11 -0500

    tensorflow RMSPropOptimizer with minimize loss

--- supervised_learning/0x03-optimization/8-RMSProp.py
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -9,10 +9,17 @@ def create_RMSProp_op(loss, alpha, beta2, epsilon):
     """
     creates the training operation for a neural network
     in tensorflow using the RMSProp optimization algorithm
+    - loss: loss of the network
+    - alpha: learning rate
+    - beta2: RMSProp weight
+    - epsilon: small number to avoid division by zero
+    Returns: the RMSProp optimization operation
     """
-    optimizer = tf.train.RMSPropOptimizer(alpha,
+    # Construct a new RMSProp optimizer
+    rmsprop = tf.train.RMSPropOptimizer(alpha,
                                           decay=beta2,
                                           epsilon=epsilon)
-    rmsprop = optimizer.minimize(loss)
+    # minimize combines calls compute_gradients() and apply_gradients()
+    minimize = rmsprop.minimize(loss)
 
-    return rmsprop
+    return minimize
