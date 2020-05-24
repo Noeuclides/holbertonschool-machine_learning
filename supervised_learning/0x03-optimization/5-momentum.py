75e94f1dbf8823395452b478d8bb2e3fdcac10a0
Noeuclides <euclidesnoeuclides@gmail.com>
2020-05-24 11:34:36 -0500

    update variables with momentum

--- supervised_learning/0x03-optimization/5-momentum.py
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -9,6 +9,14 @@ def update_variables_momentum(alpha, beta1, var, grad, v):
     """
     updates a variable using the gradient descent
     with momentum optimization algorithm
+    - alpha: learning rate
+    - beta1: momentum weight
+    - var: numpy.ndarray containing the variable to be updated
+    - grad: numpy.ndarray containing the gradient of var
+    - v: previous first moment of var
+    Returns: the updated variable and the new moment, respectively
+    Vdw = ß * Vdw + (1 - ß) * dW
+    W = W - alpha * Vdw
     """
     v = v * beta1 + (1 - beta1) * grad
     var = var - alpha * v
