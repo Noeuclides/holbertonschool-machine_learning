1e4f6fd2aeaa522e3cbfe3552b1015d6de3e4e68
Noeuclides <euclidesnoeuclides@gmail.com>
2020-05-24 14:02:27 -0500

    update varaibles with RMSprop

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -6,11 +6,18 @@ import numpy as np
 
 
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
-    """updates a variable using the RMSProp optimization algorithm
     """
-    rms = grad * grad
-    s = beta2 * s + (1 - beta2) * rms
-    den = s ** (1 / 2) + epsilon
-    var = var - alpha * grad / den
+    updates a variable using the RMSProp optimization algorithm
+    - alpha: learning rate
+    - beta2: RMSProp weight
+    - epsilon: small number to avoid division by zero
+    - var is a numpy.ndarray containing the variable to be updated
+    - grad: numpy.ndarray containing the gradient of var
+    - s: previous second moment of var
+    Returns: the updated variable and the new moment, respectively
+    """
+    s = beta2 * s + (1 - beta2) * grad ** 2
+    sqrt = (s + epsilon) ** (1 / 2)
+    var = var - alpha * grad / sqrt
 
     return var, s
