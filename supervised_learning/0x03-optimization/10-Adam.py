11ae935e4c82fd0e1d3360e9a0931824965bcb0e
Noeuclides <euclidesnoeuclides@gmail.com>
2020-05-24 16:13:38 -0500

    tensorflow AdamOptimizer with minimize loss

--- supervised_learning/0x03-optimization/10-Adam.py
+++ supervised_learning/0x03-optimization/10-Adam.py
@@ -9,9 +9,16 @@ def create_RMSProp_op(loss, alpha, beta2, epsilon):
     """
     creates the training operation for a neural network
     in tensorflow using the adam optimization algorithm
+    - loss: loss of the network
+    - alpha: learning rate
+    - beta1: weight used for the first moment
+    - beta2: weight used for the second moment
+    - epsilon: small number to avoid division by zero
+    Returns: the Adam optimization operation
     """
-    optimizer = tf.train.AdamOptimizer(alpha, beta1,
-                                       beta2, epsilon)
-    adam = optimizer.minimize(loss)
+    adam = tf.train.AdamOptimizer(alpha, beta1,
+                                  beta2, epsilon)
+    # combines calls compute_gradients() and apply_gradients()
+    minimize = adam.minimize(loss)
 
-    return adam
+    return minimize
