b5d38c31860d8891ee1469bcc3335b50ccd587b5
Noeuclides <euclidesnoeuclides@gmail.com>
2020-05-25 13:32:05 -0500

    batch normalization

--- supervised_learning/0x03-optimization/12-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -16,7 +16,7 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     the learning rate decay occurs in a stepwise fashion
     Returns: the learning rate decay operation
     """
-    learning_rate_decay = tf.train.inverse_time_decay(
+    lr_decay = tf.train.inverse_time_decay(
         alpha, global_step, decay_step, decay_rate, staircase=True)
 
-    return learning_rate_decay
+    return lr_decay
--- supervised_learning/0x03-optimization/13-batch_norm.py
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python3
 """
+module to batch normalization
 """
-import tensorflow as tf
+
 import numpy as np
+
+
+def batch_norm(Z, gamma, beta, epsilon):
+    """
+    normalizes an unactivated output of a neural network using BN:
+
+    - Z: numpy.ndarray of shape (m, n) that should be normalized
+        - m: number of data points
+        - n: number of features in Z
+    - gamma: numpy.ndarray of shape (1, n) containing the scales used for BN
+    - beta: numpy.ndarray of shape (1, n) containing the offsets used for BN
+    - epsilon: small number used to avoid division by zero
+    Returns: the normalized Z matrix
+    """
+    mean = np.sum(Z, axis=0) / Z.shape[0]
+    variance = np.sum((Z - mean)**2, axis=0) / Z.shape[0]
+
+    Znorm = (Z - mean) / (variance + epsilon) ** (1/2)
+
+    batch_norm = gamma * Znorm + beta
+
+    return batch_norm
