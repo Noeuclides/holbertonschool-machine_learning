a7b17c2b94da2d5d2a293e1b7c47d7d7beb07e9c
Noeuclides <euclidesnoeuclides@gmail.com>
2020-05-24 19:06:01 -0500

    tensorflow inverse time decay

--- supervised_learning/0x03-optimization/12-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -7,11 +7,16 @@ import tensorflow as tf
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """
-    creates the training operation for a neural network
-    in tensorflow using the learning decay optimization algorithm
+    creates a learning rate decay operation in tf using inverse time decay
+    - alpha: original learning rate
+    - decay_rate: weight used to determine the rate at which alpha will decay
+    - global_step: number of passes of gradient descent that have elapsed
+    - decay_step: number of passes of gradient descent that occurs before
+    alpha is decayed further
+    the learning rate decay occurs in a stepwise fashion
+    Returns: the learning rate decay operation
     """
-    optimizer = tf.train.inverse_time_decay(
+    learning_rate_decay = tf.train.inverse_time_decay(
         alpha, global_step, decay_step, decay_rate, staircase=True)
-    decay = optimizer.minimize(loss)
 
-    return decay
+    return learning_rate_decay
