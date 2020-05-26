4c5a35231241490cc4c4a84a7e84b8f14a15948c
Noeuclides <euclidesnoeuclides@gmail.com>
2020-05-26 09:18:37 -0500

    sensitivity

--- supervised_learning/0x04-error_analysis/1-sensitivity.py
+++ supervised_learning/0x04-error_analysis/1-sensitivity.py
@@ -6,10 +6,14 @@ import numpy as np
 
 
 def sensitivity(confusion):
-    """calculates the sensitivity for each class in a confusion matrix
     """
-
-    true_pos = np.diagonal(confusion)
-    sensitivity = true_pos / np.sum(confusion, axis=1)
+    calculates the sensitivity for each class in a confusion matrix
+    - confusion: numpy.ndarray of shape (classes, classes) where row indices
+    represent the correct labels and column indices represent the predicted labels
+        - classes: number of classes
+    Returns: a numpy.ndarray of shape (classes,) containing the sensitivity of each class
+    """
+    true_positive = confusion.diagonal()
+    sensitivity = true_positive / np.sum(confusion, axis=1)
 
     return sensitivity
