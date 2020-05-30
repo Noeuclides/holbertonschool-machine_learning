952f156ab9e6c31f442733a6217834fe45e09a74
Noeuclides <euclidesnoeuclides@gmail.com>
2020-05-29 23:26:39 -0500

    tensor l2 regularizer cost with tf.losses.get_regularization_losses instead of contrib.layers.l2_regularizer

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -12,6 +12,6 @@ def l2_reg_cost(cost):
     Returns: a tensor containing the cost of the network accounting
     for L2 regularization
     """
-    l2_reg_cost = cost + tf.contrib.layers.l2_regularizer(scale=0.1)
+    l2_reg_cost = cost + tf.losses.get_regularization_losses()
 
     return l2_reg_cost
