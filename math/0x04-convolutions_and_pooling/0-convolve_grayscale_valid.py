75f1671e1430df6324010c4c0c72596cf317d32e
Noeuclides <euclidesnoeuclides@gmail.com>
2020-06-11 18:08:11 -0500

    improve some lines on convolve valid

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -11,17 +11,17 @@ def convolve_grayscale_valid(images, kernel):
     - images: numpy.ndarray, shape (m, h, w), with multiple grayscale images
     - kernel: numpy.ndarray, shape (kh, kw), with the kernel for the conv.
     """
-    step_h = kernel.shape[0]
-    step_w = kernel.shape[1]
-    window_h = images.shape[1] - kernel.shape[0] + 1
-    window_w = images.shape[2] - kernel.shape[1] + 1
-    m = images.shape[0]
-    img = np.arange(m)
-    convolution = np.zeros((m, window_h, window_w))
-    for i in range(window_h):
-        for j in range(window_w):
-            convole = images[img, i:i + step_h, j:j + step_w] * kernel
-            pos = np.sum(convole, axis=(1, 2))
-            convolution[img, i, j] = pos
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    out_w = w - kw + 1
+    out_h = h - kh + 1
+    convolve_out = np.ndarray((m, out_h, out_w))
 
-    return convolution
+    for i in range(out_h):
+        for j in range(out_w):
+            convolve_out[:, i, j] = np.sum(
+                images[:, i:kh+i, j:kw+j] * kernel,
+                axis=(1, 2)
+                )
+
+    return convolve_out
