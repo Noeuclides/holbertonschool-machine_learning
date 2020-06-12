8236e94fd66e7125e84603727787b477b1bc8adb
Noeuclides <euclidesnoeuclides@gmail.com>
2020-06-11 20:57:44 -0500

    convolve same with even kernel size case

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -13,13 +13,22 @@ def convolve_grayscale_same(images, kernel):
     """
     m, h, w = images.shape
     kh, kw = kernel.shape
-    # get padding
+
     ph = int((kh - 1) / 2)
     pw = int((kw - 1) / 2)
-    out_w = w + 2*pw - kw + 1
-    out_h = h + 2*ph - kh + 1
-    # new images with padding
-    images_pad = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant')
+
+    if kh % 2 != 0:
+        out_w = w + 2*pw - kw + 1
+        out_h = h + 2*ph - kh + 1
+        images_pad = np.pad(
+            images, ((0, 0), (ph, ph), (pw, pw)), 'constant'
+            )
+    else:
+        out_w = w + 2*pw - kw + 2
+        out_h = h + 2*ph - kh + 2
+        images_pad = np.pad(
+            images, ((0, 0), (0, ph + 1), (0, pw + 1)), 'constant'
+            )
 
     convolve_out = np.ndarray((m, out_h, out_w))
 
@@ -27,7 +36,6 @@ def convolve_grayscale_same(images, kernel):
         for j in range(out_w):
             convolve_out[:, i, j] = np.sum(
                 images_pad[:, i:kh+i, j:kw+j] * kernel,
-                axis=(1, 2)
-                )
+                axis=(1, 2))
 
     return convolve_out
