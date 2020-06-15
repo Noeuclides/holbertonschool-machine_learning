3312e95f6d51075fe89310b979fc4e6bc0d9c58f
Noeuclides <euclidesnoeuclides@gmail.com>
2020-06-15 08:28:37 -0500

    lenet with tf: convolution layers with activation function

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -10,31 +10,40 @@ def lenet5(x, y):
     builds a modified version of the LeNet-5 architecture using tensorflow
     """
     initializer = tf.contrib.layers.variance_scaling_initializer()
+    activation = tf.nn.relu
 
+    # convolution section
     conv_layer1 = tf.layers.Conv2D(
-        filters=6, kernel_size=(
-            5, 5), padding='same',
-        kernerl_initializer=initializer)(x)
-    max_pool = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=2)(conv_layer1)
+        filters=6, kernel_size=(5, 5), padding='same',
+        kernel_initializer=initializer,
+        activation=activation)(x)
+    max_pool = tf.layers.MaxPooling2D(pool_size=(2, 2),
+                                      strides=(2, 2))(conv_layer1)
     conv_layer2 = tf.layers.Conv2D(
-        filters=16, kernel_size=(
-            5, 5), padding='valid',
-        kernel_initializer=initializer)(max_pool)
-    max_pool = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=2)(conv_layer2)
+        filters=16, kernel_size=(5, 5), padding='valid',
+        kernel_initializer=initializer,
+        activation=activation)(max_pool)
+    max_pool = tf.layers.MaxPooling2D(pool_size=(2, 2),
+                                      strides=(2, 2))(conv_layer2)
+
+    # Fully connected section
     flatten = tf.layers.Flatten()(max_pool)
-    layer = tf.layers.Dense(units=120, activation='relu',
+    layer = tf.layers.Dense(units=120, activation=activation,
                             kernel_initializer=initializer)(flatten)
-    layer = tf.layers.Dense(units=84, activation='relu',
+    layer = tf.layers.Dense(units=84, activation=activation,
                             kernel_initializer=initializer)(layer)
-    layer = tf.layers.Dense(units=10, activation='relu',
+    layer = tf.layers.Dense(units=10,
                             kernel_initializer=initializer)(layer)
-    layer = tf.nn.softmax(layer)
+
+    # prediction performance
+    output = tf.nn.softmax(layer)
     loss = tf.losses.softmax_cross_entropy(y, layer)
     train = tf.train.AdamOptimizer().minimize(loss)
 
-    val = tf.argmax(y, 1)
-    pred = tf.argmax(layer, 1)
-    eq = tf.equal(pred, val)
-    acc = tf.reduce_mean(tf.cast(eq, tf.float32))
+    # metrics
+    label = tf.argmax(y, 1)
+    prediction = tf.argmax(layer, 1)
+    compare = tf.equal(prediction, label)
+    accuracy = tf.reduce_mean(tf.cast(compare, tf.float32))
 
-    return (layer, train, loss, acc)
+    return (output, train, loss, accuracy)
