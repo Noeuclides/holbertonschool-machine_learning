61ae5ec9c6d0f9eba3b0deb298250a22e66858f0
Noeuclides <euclidesnoeuclides@gmail.com>
2020-10-22 23:06:10 -0500

    linear algebra: minor matrix of a matrix II, change order of errors

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -10,6 +10,15 @@ def determinant(matrix: list) -> int:
     - matrix: list of lists whose determinant should be calculated
     Returns: the determinant of the matrix
     """
+    if len(matrix) == 1:
+        if len(matrix[0]) == 0:
+            return 1
+        elif len(matrix[0]) == 1:
+            return matrix[0][0]
+    for elem in matrix:
+        # check if it's a square matrix
+        if len(elem) != len(matrix):
+            raise ValueError("matrix must be a non-empty square matrix")
     if len(matrix) == 2:
         return two_by_two_det(matrix)
 
@@ -52,6 +61,9 @@ def minor(matrix: list) -> list:
     """
     if not isinstance(matrix, list) or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
+    for elem in matrix:
+        if not isinstance(elem, list):
+            raise TypeError("matrix must be a list of lists")
     if len(matrix) == 1:
         if len(matrix[0]) == 1:
             return [[1]]
@@ -61,8 +73,6 @@ def minor(matrix: list) -> list:
         # check if it's a square matrix
         if len(elem) != len(matrix):
             raise ValueError("matrix must be a non-empty square matrix")
-        if not isinstance(elem, list):
-            raise TypeError("matrix must be a list of lists")
     if len(matrix) == 2:
         return [
             [matrix[1][1], matrix[1][0]],
