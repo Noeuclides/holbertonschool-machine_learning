dff782cc4c81e9fd544a30a1f01bf0d7af8c2dc0
Noeuclides <euclidesnoeuclides@gmail.com>
2020-10-26 22:57:00 -0500

    Multivariate Probability: correlation matrix, broadcasting

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -12,17 +12,15 @@ def correlation(C):
     - C: numpy.ndarray of shape (d, d) containing a covariance matrix
         - d: number of dimensions
     Returns a numpy.ndarray of shape (d, d) containing the correlation matrix
+
+    rij = Cij / sqrt(Cii * Cjj)
     """
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
     if len(C.shape) != 2:
         raise ValueError("C must be a 2D square matrix")
 
-    r = np.zeros(shape=C.shape)
-    for i in range(C.shape[0]):
-        for j in range(C.shape[1]):
-            den_coef1 = C[i][i] ** (1 / 2)
-            den_coef2 = C[j][j] ** (1 / 2)
-            r[i][j] = C[i][j] / (den_coef1 * den_coef2)
+    diag = np.diag(C).reshape(1, C.shape[0])
+    r = C / np.sqrt(diag.T * diag)
 
     return r
