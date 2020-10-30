66aa6219695d4ee4470c5aed01570ecfa2f242f3
Noeuclides <euclidesnoeuclides@gmail.com>
2020-10-29 20:28:35 -0500

    pca

---
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module to perform Principal component Analisys
+"""
+import numpy as np
+
+
+def pca(X, ndim):
+    """
+    performs PCA on a dataset
+    - X: numpy.ndarray of shape (n, d) where:
+        - n: number of data points
+        - d: number of dimensions in each point
+    - ndim: new dimensionality of the transformed X
+    Returns: T, a numpy.ndarray of shape (n, ndim) containing the
+    transformed version of X
+    """
+    u, s, _ = np.linalg.svd(X)
+    diag = np.diag(s[:ndim])
+    T = np.matmul(u[:ndim].T, diag)
+
+    return T
