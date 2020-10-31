e08b60bafd2550e1c46b405a81316747d46b9c17
Noeuclides <euclidesnoeuclides@gmail.com>
2020-10-31 00:14:51 -0500

    conditions

--- math/0x07-bayesian_prob/2-marginal.py
+++ math/0x07-bayesian_prob/2-marginal.py
@@ -31,15 +31,15 @@ def marginal(x, n, P, Pr):
             "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if len(P.shape) != 1:
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    if not isinstance(Pr, np.ndarray):
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
-    if any([False for i in P if i < 0 or i > 1]):
+    if np.amin(P) < 0 or np.amax(P) > 1:
         raise ValueError("All values in P must be in the range [0, 1]")
-    if any([False for i in Pr if i < 0 or i > 1]):
+    if np.amin(Pr) < 0 or np.amax(Pr) > 1:
         raise ValueError("All values in Pr must be in the range [0, 1]")
-    if not np.isclose(np.sum(Pr), 1):
+    if not np.isclose([np.sum(Pr)], [1])[0]:
         raise ValueError("Pr must sum to 1")
 
     fac = np.math.factorial
