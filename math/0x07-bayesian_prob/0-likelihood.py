620f82c2dd5a3f15c68b89b17b7935ab41505bd3
Noeuclides <euclidesnoeuclides@gmail.com>
2020-10-30 23:55:18 -0500

    check ndarray

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -28,7 +28,7 @@ def likelihood(x, n, P):
             "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if len(P.shape) != 1:
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
     if any([False for i in P if i < 0 or i > 1]):
         raise ValueError("All values in P must be in the range [0, 1]")
