d998f1044d1aa643ac8f3cd7a2553b22f2214ddc
Noeuclides <euclidesnoeuclides@gmail.com>
2020-11-09 11:32:09 -0500

    -_-

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -18,7 +18,8 @@ def initialize(X, k):
     Returns: a numpy.ndarray of shape (k, d) containing the initialized
     centroids for each cluster, or None on failure
     """
-    if not isinstance(X, np.ndarray) or X.shape != 2:
+    print(X.shape)
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None
     if not isinstance(k, int) or k <= 0:
         return None
