66d8ec0ac08010fe2139e98e256a51a7eeb32446
Noeuclides <euclidesnoeuclides@gmail.com>
2020-11-25 15:15:30 -0500

    autoencoder vanilla

--- unsupervised_learning/0x04-autoencoders/0-vanilla.py
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -2,7 +2,6 @@
 """
 Vanilla Autoencoder module
 """
-
 import numpy as np
 import tensorflow as tf
 
@@ -22,4 +21,29 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
         - decoder: decoder model
         - auto: full autoencoder model
     """
-    pass
+    # encoder
+    encoder_input = tf.keras.Input(shape=(input_dims,))
+    encoder = tf.keras.layers.Dense(hidden_layers[0],
+                                    activation='relu')(encoder_input)
+    for layer in hidden_layers[1:]:
+        encoder = tf.keras.layers.Dense(layer, activation='relu')(encoder)
+    encoder = tf.keras.layers.Dense(latent_dims, activation='relu')(encoder)
+    encoder = tf.keras.Model(inputs=encoder_input, outputs=encoder)
+
+    # decoder
+    decoder_input = tf.keras.Input(shape=(latent_dims,))
+    decoder = tf.keras.layers.Dense(hidden_layers[-1],
+                                    activation='relu')(decoder_input)
+    for layer in reversed(hidden_layers[:-1]):
+        decoder = tf.keras.layers.Dense(layer, activation='relu')(decoder)
+    decoder = tf.keras.layers.Dense(input_dims, activation='sigmoid')(decoder)
+    decoder = tf.keras.Model(inputs=decoder_input, outputs=decoder)
+
+    # autoencoder
+    encoder_output = encoder(encoder_input)
+    decoder_output = decoder(encoder_output)
+    auto = tf.keras.Model(inputs=encoder_input, outputs=decoder_output)
+
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
